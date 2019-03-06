39cf2b67a9a89f6c5c8d593af21d8835a6e0aae1
huwenbo <huwenbo518@163.com>
2019-03-06 23:24:09 +0200

    Define the interface.

--- MouseEvent/qvcvmouseevent.cpp
+++ MouseEvent/qvcvmouseevent.cpp
@@ -2,5 +2,30 @@
 
 QVCVMouseEvent::QVCVMouseEvent(QWidget *parent) : QWidget(parent)
 {
+    data = nullptr;
+}
 
+QVCVMouseEvent::~QVCVMouseEvent()
+{
+
+}
+
+void QVCVMouseEvent::MousePressEvent(QMouseEvent *event,QPaintDevice *device)
+{
+
+}
+
+void QVCVMouseEvent::MouseMoveEvent(QMouseEvent *event,QPaintDevice *device)
+{
+
+}
+
+void QVCVMouseEvent::MouseReleseEvent(QMouseEvent *event,QPaintDevice *device)
+{
+
+}
+
+QVCVMouseEventData* QVCVMouseEvent::GetData()
+{
+    return nullptr;
 }
--- MouseEvent/qvcvmouseevent.h
+++ MouseEvent/qvcvmouseevent.h
@@ -3,15 +3,29 @@
 
 #include <QWidget>
 
+class QVCVMouseEventData
+{
+
+};
+
 class QVCVMouseEvent : public QWidget
 {
     Q_OBJECT
 public:
     explicit QVCVMouseEvent(QWidget *parent = nullptr);
-
+    ~QVCVMouseEvent();
 signals:
 
 public slots:
+
+public:
+    virtual void MousePressEvent(QMouseEvent *event,QPaintDevice *device);
+    virtual void MouseMoveEvent(QMouseEvent *event,QPaintDevice *device);
+    virtual void MouseReleseEvent(QMouseEvent *event,QPaintDevice *device);
+
+    QVCVMouseEventData* GetData();
+protected:
+    QVCVMouseEventData *data;
 };
 
 #endif // QVCVMOUSEEVENT_H
